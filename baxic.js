15936f69a54d74bf32264d8cc0fece411f59974e
Ayomide Idowu <idowuayomide315@gmail.com>
2025-06-26 04:03:45 +0100

    all files

---
+++ baxic.js
@@ -0,0 +1,10 @@
+class person{
+    constructor(firstname, lastname){
+        this.firstname = firstname
+        this.lastname = lastname
+    }
+      
+}
+
+const person = new person("john", "doe")
+console.log(person)
